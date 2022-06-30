d3f2c21e67392654b9171bb8bce1850599280fd8
reginaguermandi <reginaguermandi@gmail.com>
2022-06-30 16:22:05 -0300

    add reset after calculate

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const operatorKey = document.querySelector('[data-type="operator"]');
 
 
 keys.addEventListener('click', event => {
-  if(!event.target.closest('button')) return
+  if (!event.target.closest('button')) return
 
-// print the Key value in the display
+  // print the Key value in the display
   const key = event.target
   const keyValue = key.textContent
   const displayValue = display.textContent
@@ -15,45 +15,49 @@ keys.addEventListener('click', event => {
   const { previousKeyType } = calculator.dataset
   display.textContent = keyValue
 
-
- if(type === 'number'){
-    if(displayValue === '0' || previousKeyType === 'operator'){
+  // print only numbers in the display
+  if (type === 'number') {
+    if (displayValue === '0' || previousKeyType === 'operator' || previousKeyType === 'equal') {
       display.textContent = keyValue
     } else {
       display.textContent = displayValue + keyValue
-    }       
-  } 
+    }
+  }
 
+  // store the first input and an operator
   if (type === 'operator') {
     calculator.dataset.firstNumber = displayValue
     calculator.dataset.operator = key.dataset.key
   }
 
+  // store the second input and perfom a calculation
   if (type === 'equal') {
-    if( previousKeyType !== 'equal'){
+    if (previousKeyType !== 'equal') {
       const firstNumber = calculator.dataset.firstNumber
       const operator = calculator.dataset.operator
       const secondNumber = displayValue
-      
-      display.textContent = calculate(firstNumber, operator, secondNumber)      
+      const result = calculate(firstNumber, operator, secondNumber)
+      display.textContent = result
     } else {
       display.textContent = displayValue
     }
-  }    
+  }
 
-  if(type === 'clear'){
+  // clear display
+  if (type === 'clear') {
     display.textContent = '0';
     delete calculator.dataset.firstNumber
     delete calculator.dataset.secondNumber
   }
 
+  // 
   if (type === 'decimal') {
     if (!displayValue.includes('.')) {
       display.textContent = displayValue + '.'
     } else {
-      display.textContent = '0'
+      display.textContent = displayValue
     }
-    
+
   }
 
   calculator.dataset.previousKeyType = type
@@ -63,10 +67,8 @@ function calculate(firstNumber, operator, secondNumber) {
   firstNumber = parseFloat(firstNumber)
   secondNumber = parseFloat(secondNumber)
 
-  if(operator === 'plus') return firstNumber + secondNumber
-  if(operator === 'minus') return firstNumber - secondNumber
-  if(operator === 'times') return firstNumber * secondNumber
-  if(operator === 'divide') return firstNumber / secondNumber
+  if (operator === 'plus') return firstNumber + secondNumber
+  if (operator === 'minus') return firstNumber - secondNumber
+  if (operator === 'times') return firstNumber * secondNumber
+  if (operator === 'divide') return firstNumber / secondNumber
 }
-
-
